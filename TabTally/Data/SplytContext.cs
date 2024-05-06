fbcce553f6da3d7055683fb0ea36d34a948dad15
inathan44 <inathan44@yahoo.com>
2024-05-05 20:01:28 -0400

    fix: update table names, cascade on delete

--- TabTally/Data/SplytContext.cs
+++ TabTally/Data/SplytContext.cs
@@ -9,30 +9,34 @@ public class SplytContext : DbContext
     public DbSet<Transaction> Transaction { get; set; }
     public DbSet<User> User { get; set; }
     public DbSet<Group> Group { get; set; }
-    public DbSet<GroupMembers> GroupMembers { get; set; }
-    public DbSet<TransactionDetails> TransactionDetails { get; set; }
+    public DbSet<GroupMember> GroupMember { get; set; }
+    public DbSet<TransactionDetail> TransactionDetail { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql($"Host={Environment.GetEnvironmentVariable("DB_HOST")};Database={Environment.GetEnvironmentVariable("DB_NAME")};Username={Environment.GetEnvironmentVariable("DB_USER")};Password={Environment.GetEnvironmentVariable("DB_PASS")};");
+        optionsBuilder.UseNpgsql($"Host={Environment.GetEnvironmentVariable("DB_HOST")};Database={Environment.GetEnvironmentVariable("DB_NAME")};Username={Environment.GetEnvironmentVariable("DB_USER")};Password={Environment.GetEnvironmentVariable("DB_PASS")};Include Error Detail=true");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<GroupMembers>().Property(e => e.Status).HasConversion<string>();
+        modelBuilder.Entity<GroupMember>().Property(e => e.Status).HasConversion<string>();
 
         // Configure the relationship between User/Group/GroupMembers
-        modelBuilder.Entity<GroupMembers>()
+        // Might need to change the cascade delete behavior for if a user deletes their account
+        modelBuilder.Entity<GroupMember>()
             .HasOne(gm => gm.Member)
             .WithMany(u => u.GroupMembers)
             .HasForeignKey(gm => gm.MemberId)
-            .OnDelete(DeleteBehavior.Restrict);
+            .OnDelete(DeleteBehavior.Cascade);
 
-        modelBuilder.Entity<GroupMembers>()
+        modelBuilder.Entity<GroupMember>()
             .HasOne(gm => gm.InvitedBy)
             .WithMany()
             .HasForeignKey(gm => gm.InvitedById)
-            .OnDelete(DeleteBehavior.Restrict);
+            .OnDelete(DeleteBehavior.Cascade);
+
+
+
     }
 
     public bool TestConnection()
